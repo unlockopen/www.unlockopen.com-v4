1560c237394e19f4ae01a2964c7e3fc8eebd1fec
madrilene <hola@lenesaile.com>
2023-11-28 16:08:01 +0100

    flexible sidebar

---
+++ src/_includes/components/reusables/table-saw.js
@@ -0,0 +1,149 @@
+class Tablesaw extends HTMLElement {
+  static identifiers = {};
+
+  constructor() {
+    super();
+
+    this.autoOffset = 50;
+    this._needsStylesheet = true;
+
+    this.attrs = {
+      breakpoint: 'breakpoint',
+      breakpointBackwardsCompat: 'media',
+      type: 'type',
+      ratio: 'ratio',
+      label: 'data-tablesaw-label',
+      zeropad: 'zero-padding',
+      forceTextAlign: 'text-align'
+    };
+
+    this.defaults = {
+      breakpoint: '(max-width: 39.9375em)', // same as Filament Group’s Tablesaw
+      ratio: '1fr 2fr'
+    };
+
+    this.classes = {
+      wrap: 'tablesaw-wrap'
+    };
+
+    this.props = {
+      ratio: '--table-saw-ratio'
+    };
+  }
+
+  generateCss(breakpoint, type) {
+    return `
+table-saw.${this._identifier} {
+	display: block;
+	${type === 'container' ? 'container-type: inline-size;' : ''}
+}
+
+@${type} ${breakpoint} {
+	table-saw.${this._identifier} thead :is(th, td) {
+		position: absolute;
+		height: 1px;
+		width: 1px;
+		overflow: hidden;
+		clip: rect(1px, 1px, 1px, 1px);
+	}
+	table-saw.${this._identifier} :is(tbody, tfoot) tr {
+		display: block;
+		margin-bottom: 1em;
+	}
+	table-saw.${this._identifier} :is(tbody, tfoot) :is(th, td):before {
+		font-weight: bold;
+		content: attr(${this.attrs.label});
+	}
+	table-saw.${this._identifier} :is(tbody, tfoot) :is(th, td) {
+		display: grid;
+		gap: 0 1em;
+		grid-template-columns: var(--table-saw-ratio, ${this.defaults.ratio});
+	}
+	table-saw.${this._identifier}[${
+      this.attrs.forceTextAlign
+    }] :is(tbody, tfoot) :is(th, td) {
+		text-align: ${this.getAttribute(this.attrs.forceTextAlign) || 'left'};
+	}
+	table-saw.${this._identifier}[${this.attrs.zeropad}] :is(tbody, tfoot) :is(th, td) {
+		padding-left: 0;
+		padding-right: 0;
+	}
+}`;
+  }
+
+  connectedCallback() {
+    // Cut the mustard
+    // https://caniuse.com/mdn-api_cssstylesheet_replacesync
+    if (!('replaceSync' in CSSStyleSheet.prototype)) {
+      return;
+    }
+
+    this.addHeaders();
+    this.setRatio();
+
+    if (!this._needsStylesheet) {
+      return;
+    }
+
+    let sheet = new CSSStyleSheet();
+    let breakpoint =
+      this.getAttribute(this.attrs.breakpoint) ||
+      this.getAttribute(this.attrs.breakpointBackwardsCompat) ||
+      this.defaults.breakpoint;
+    let type = this.getAttribute(this.attrs.type) || 'media';
+
+    this._identifier = `ts_${type.slice(0, 1)}${breakpoint.replace(/[^a-z0-9]/gi, '_')}`;
+    this.classList.add(this._identifier);
+
+    if (!Tablesaw.identifiers[this._identifier]) {
+      let css = this.generateCss(breakpoint, type);
+      sheet.replaceSync(css);
+
+      document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+
+      Tablesaw.identifiers[this._identifier] = true;
+    }
+  }
+
+  addHeaders() {
+    let labels = Array.from(this.querySelectorAll('thead th')).map(entry =>
+      entry.innerText.trim()
+    );
+    if (labels.length === 0) {
+      this._needsStylesheet = false;
+      console.error('No `<th>` elements for Tablesaw were found:', this);
+      return;
+    }
+
+    let cells = this.querySelectorAll('tbody :is(td, th)');
+    for (let cell of cells) {
+      if (!labels[cell.cellIndex]) {
+        continue;
+      }
+
+      cell.setAttribute(this.attrs.label, labels[cell.cellIndex]);
+
+      // wrap if this cell has child nodes for correct grid alignment
+      if (cell.firstElementChild) {
+        let wrapper = document.createElement('div');
+        wrapper.classList.add(this.classes.wrap);
+        while (cell.firstChild) {
+          wrapper.appendChild(cell.firstChild);
+        }
+        cell.appendChild(wrapper);
+      }
+    }
+  }
+
+  setRatio() {
+    let ratio = this.getAttribute(this.attrs.ratio);
+    if (ratio) {
+      let ratioString = ratio.split('/').join('fr ') + 'fr';
+      this.style.setProperty(this.props.ratio, ratioString);
+    }
+  }
+}
+
+if ('customElements' in window) {
+  window.customElements.define('table-saw', Tablesaw);
+}
---
+++ src/style-guide/Atoms/table.md
@@ -0,0 +1,63 @@
+---
+title: Table
+layout: sg-wrapper
+---
+
+<table-saw text-align>
+<table>
+    <thead>
+        <tr>
+            <th>Date</th>
+            <th class="nowrap">Behavior</th>
+            <th>Vendor</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="nowrap">2017 <span class="hide-sm"><abbr title="July">Jul</abbr> 24</span></td>
+            <td class="yes"><strong>font-display</strong> Supported</td>
+            <td>Chrome (v60) was first to implement.</td>
+        </tr>
+        <tr>
+            <td class="nowrap">2017 <span class="hide-sm"><abbr title="August">Aug</abbr> 8</span></td>
+            <td class="yes"><strong>font-display</strong> Supported</td>
+            <td>Opera (v47) was second to add support.</td>
+        </tr>
+        <tr>
+            <td class="nowrap">2018 <span class="hide-sm"><abbr title="January">Jan</abbr> 22</span></td>
+            <td class="yes"><strong>font-display</strong> Supported</td>
+            <td>Firefox (v58)</td>
+        </tr>
+        <tr>
+            <td class="nowrap">2018 <span class="hide-sm"><abbr title="March">Mar</abbr> 28</span></td>
+            <td class="yes"><strong>font-display</strong> Supported</td>
+            <td>Safari (v11.1)</td>
+        </tr>
+        <tr>
+            <td class="nowrap">2018 <span class="hide-sm"><abbr title="December">Dec</abbr> 29</span></td>
+            <td class="yes"><strong>font-display</strong> Supported</td>
+            <td>Samsung Internet (v8.2)</td>
+        </tr>
+        <tr>
+            <td class="nowrap">2019 <span class="hide-sm"><abbr title="May">May</abbr> 15</span></td>
+            <td class="yes"><strong>font-display</strong> Supported</td>
+            <td>Google Fonts</td>
+        </tr>
+        <tr>
+            <td class="nowrap"><em>Not yet</em></td>
+            <td class="no"><strong>font-display</strong> Not supported</td>
+            <td>Adobe Fonts</td>
+        </tr>
+        <tr>
+            <td class="nowrap"><em>Not yet</em></td>
+            <td class="no"><strong>font-display</strong> Not supported</td>
+            <td>Typography.com (by Hoefler&Co)</td>
+        </tr>
+        <tr>
+            <td class="nowrap"><em>Not yet</em></td>
+            <td class="no"><strong>font-display</strong> Not supported</td>
+            <td>fonts.com (by Monotype)</td>
+        </tr>
+    </tbody>
+</table>
+</table-saw>
