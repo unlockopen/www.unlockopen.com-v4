acfb5db1527fdd4bdfcc615982c04b6db701717a
madrilene <hola@lenesaile.com>
2024-02-08 13:18:50 +0100

    remove misleading comment

--- src/pages/pages.11tydata.js
+++ src/pages/pages.11tydata.js
@@ -1,7 +1,6 @@
 module.exports = {
   layout: 'page',
   permalink: function (data) {
-    // Slug override for localized URL slugs
     if (data.slugOverride) {
       return `/${this.slugify(data.slugOverride)}/`;
     }
