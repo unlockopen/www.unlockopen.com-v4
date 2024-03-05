f78ce305e293a99e78090acc7c2d87d70c97a6ef
madrilene <hola@lenesaile.com>
2024-03-05 07:32:18 +0100

    use updated version of markdown-it-eleventy-img, activate captions

--- src/_eleventy/plugins/markdown.js
+++ src/_eleventy/plugins/markdown.js
@@ -4,7 +4,7 @@ import markdownItAnchor from 'markdown-it-anchor';
 import markdownItClass from '@toycode/markdown-it-class';
 import markdownItLinkAttributes from 'markdown-it-link-attributes';
 import {full as markdownItEmoji} from 'markdown-it-emoji';
-import markdownFigcaption from 'markdown-it-image-figures';
+import markdownItEleventyImg from 'markdown-it-eleventy-img';
 import markdownItFootnote from 'markdown-it-footnote';
 import markdownitMark from 'markdown-it-mark';
 import markdownitAbbr from 'markdown-it-abbr';
@@ -44,11 +44,45 @@ const markdownLib = markdownIt({
     }
   ])
   .use(markdownItEmoji)
+  .use(markdownItEleventyImg, {
+    imgOptions: {
+      widths: [440, 880, 1024],
+      urlPath: '/assets/images/',
+      outputDir: './dist/assets/images/',
+      formats: ['webp', 'jpeg']
+    },
+    globalAttributes: {
+      loading: 'lazy',
+      decoding: 'async',
+      sizes: '90vw'
+    },
+    // prepend src for markdown images
+    resolvePath: (filepath, env) => {
+      return path.join('src', filepath);
+    },
+    renderImage(image, attributes) {
+      const [Image, options] = image;
+      const [src, attrs] = attributes;
+
+      Image(src, options);
+
+      const metadata = Image.statsSync(src, options);
+      const imageMarkup = Image.generateHTML(metadata, attrs, {
+        whitespaceMode: 'inline'
+      });
+
+      const imageElement = attrs.title
+        ? `<figure class="flow">
+			${imageMarkup}
+					<figcaption>${attrs.title}</figcaption>
+				</figure>`
+        : `${imageMarkup}`;
+
+      return imageElement;
+    }
+  })
   .use(markdownItFootnote)
   .use(markdownitMark)
-  .use(markdownitAbbr)
-  .use(markdownFigcaption, {
-    figcaption: true
-  });
+  .use(markdownitAbbr);
 
 export default markdownLib;
